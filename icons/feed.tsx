ae924b6273e30035a55217c8402189830e4d115d
josephslaughter808 <josephslaughter13@gmail.com>
2026-03-31 12:57:12 -0600

    Use bulletin feed icon

--- icons/feed.tsx
+++ icons/feed.tsx
@@ -3,11 +3,12 @@ import { GlowIcon, baseStroke } from "./shared";
 export default function FeedIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <GlowIcon {...props}>
-      <path d="M6.5 17.5V8.7C6.5 7.8 7.2 7 8.2 7H14.8C15.8 7 16.5 7.8 16.5 8.7V17.5" style={baseStroke} />
-      <path d="M9 7V5.8C9 5 9.7 4.4 10.5 4.4H13.5C14.3 4.4 15 5 15 5.8V7" style={baseStroke} />
-      <path d="M4.8 11.5H18.2" style={baseStroke} />
-      <path d="M11.5 11.5V14.6" style={baseStroke} />
-      <path d="M9.8 14.6H13.2" style={baseStroke} />
+      <path d="M6.2 7.2H17.8V17.8H6.2V7.2Z" style={baseStroke} />
+      <path d="M9.2 5.4V7.2" style={baseStroke} />
+      <path d="M14.8 5.4V7.2" style={baseStroke} />
+      <path d="M8.8 10.4H15.2" style={baseStroke} />
+      <path d="M8.8 13.1H15.2" style={baseStroke} />
+      <path d="M8.8 15.8H12.7" style={baseStroke} />
     </GlowIcon>
   );
 }
